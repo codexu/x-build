74de879604ae60c4a0dd35ebaf01b0c20d5eb28f
codexu <461229187@qq.com>
2017-03-13 11:30:49 +0800

    add build task
    增加build事件，压缩JS/CSS/HTML/图片

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@
 /*  工具基本库  */
 const gulp = require('gulp')                    // 引入gulp基础库
 const watch = require('gulp-watch')             // 监听
-const rename = require("gulp-rename")           // 重命名
-const package = require('./package.json')       // 引入package
 const plumber = require('gulp-plumber');        // 防止编译错误报错终止监听
+
+/*  htmlmin  */
+const htmlmin = require('gulp-htmlmin');
 /*  css  */
 const minifyCSS = require('gulp-minify-css')    // css压缩
 const sass = require('gulp-ruby-sass')          // sass编译
@@ -42,11 +43,52 @@ gulp.task('js', () => {
     }))
     .pipe(gulp.dest(DIST_PATH + '/js'))
 })
-// 压缩图片
+/*  压缩图片  */
 gulp.task('images', () => {
   gulp.src('src/images/*.{png,jpg,gif,ico,JPG,PNG,GIF,ICO}')
     .pipe(imagemin())
     .pipe(gulp.dest(DIST_PATH + '/images'))
+})
+
+/*  build  */
+gulp.task('build', ()=> {
+  // ES6编译压缩
+  gulp.src('./src/js/app.js')
+    .pipe(plumber())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(BUILD_PATH + '/js'))
+
+  // html压缩
+  const HTML_OPTIONS = {
+    collapseWhitespace:true,
+    collapseBooleanAttributes:true,
+    removeComments:true,
+    removeEmptyAttributes:true,
+    removeScriptTypeAttributes:true,
+    removeStyleLinkTypeAttributes:true,
+    minifyJS:true,
+    minifyCSS:true
+  }
+  gulp.src('./src/*.html')
+    .pipe(htmlmin(HTML_OPTIONS))
+    .pipe(gulp.dest(BUILD_PATH))
+
+  // 图片压缩
+  gulp.src('src/images/*.{png,jpg,gif,ico,JPG,PNG,GIF,ICO}')
+  .pipe(imagemin())
+  .pipe(gulp.dest(BUILD_PATH + '/images'))
+
+  // sass编译压缩
+  return sass('./src/css/app.scss')
+    .pipe(plumber())
+	  .on('error', function (err) {
+      console.error('Error!', err.message)
+    })
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(BUILD_PATH + '/css'))
 })
 
 // 自动监听
