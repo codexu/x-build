8ec46071bd788d241289fdfe82c5bed30022eecd
李旭 <461229187@qq.com>
2017-12-27 13:51:25 +0800

    Rem 布局

---
+++ src/scripts/rem.js
@@ -0,0 +1,16 @@
+function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth
+      if (!clientWidth) return
+      if (clientWidth >= 750) {
+        docEl.style.fontSize = '100px'
+      } else {
+        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px'
+      }
+    }
+  if (!doc.addEventListener) return
+  win.addEventListener(resizeEvt, recalc, false)
+  doc.addEventListener('DOMContentLoaded', recalc, false)
+})(document, window)
