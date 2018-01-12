4e69659867f577818df7a3166a4ed79372897e32
李旭 <461229187@qq.com>
2018-01-12 16:46:08 +0800

    修改文档

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,5 @@
+import { clearImmediate } from 'timers';
+
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
