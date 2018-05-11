805d29a88fa367a8072214e793e61dad1a5fcd3c
李旭 <461229187@qq.com>
2018-05-11 17:37:56 +0800

    v4.2.0 升级到webpack4.8.1

--- README.md
+++ README.md
@@ -1,13 +1,83 @@
 # x-build 4.2.0
 
-### 开发模式
-通过命令：`npm run dev`启动本地`Web Server(http://localhost:3000/)`，默认监听端口 3000，并实时监听文件，发生改动时，浏览器会自动热更新。
+### 前言
+
+x-build v4.2.0 已完成webpack4.8.1升级。
+
+### 起步
+```
+  git clone https://github.com/codexu/x-build.git
+  cd x-build
+  npm install
+  /* 网速不好请使用cnpm(https://npm.taobao.org) */
+  npm run dev
+  /* webpack-dev-server监听3000端口(http://localhost:3000/)，支持HMR热更新 */
+```
 
 ### 生产模式
-通过命令：`npm run build` 对整个项目进行打包，目录（./output）。
+```
+  npm run build
+  /* js编译打包、css分离、第三方库抽离 */
+```
 
-### 功能&风格
-- 使用`pug`作为模版引擎。
-- 使用`stylus`作为css预处理语言。
-- 使用`babel`编译es6代码。
-- 图片进行压缩处理，对`8kb`以下的图片进行`base64`处理。
+### 风格指南
+
+> html采用pug模板引擎（原名jade），使用缩进的代码风格。 >>> [参考文档](https://pug.bootcss.com/api/getting-started.html)
+
+```pug
+  //- pug模版引擎
+  html(lang="en")
+  head
+    meta(charset="UTF-8")
+    title x-build
+  body
+    include ./components/x-build.pug
+```
+
+> css预处理器采用stylus，代码风格类似sass。 >>> [参考文档](http://stylus-lang.com/)
+
+```stylus
+@import "../utils/reset.styl"
+.container
+  width 300px
+  img
+    width 150px
+    height 150px
+  h1
+    margin-top 20px
+```
+
+> 支持bable编译ES6
+
+```javascript
+class xLoader {
+  constructor(opts) {
+    this.wrapper = opts.wrapper
+  }
+  init() {
+    console.log('do something...')
+  }
+}
+```
+
+### 功能模块
+
+> pug、stylus、es6编译压缩
+
+> css autoprefixer 前缀自动补全
+
+> [px2rem-loader + hotcss.js 自适应布局解决方案](https://github.com/codexu/Issues/issues/11)
+
+> css代码抽离、第三方库抽离
+
+> webpack-dev-server 支持HMR热更新
+
+> source-map更快定位源码
+
+> base64处理小于8kb图片
+
+> hash命名
+
+### 插件
+
+> [x-loader图片加载loading控制插件](https://github.com/codexu/Issues/issues/12)
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,22 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const config = {
-  // 入口
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.[hash:6].js',
+    filename: 'bundle.[hash:8].js',
     path: path.resolve(__dirname, 'dist')
   },
-  // 配置loader
   module: {
-    rules: [
-      // 编译es6
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-      },
-      // 编译jade
-      {
-        test: /\.(jade|pug)$/,
-        use: ['html-loader', 'pug-html-loader']
-      },
-      // 图片处理
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-            name: 'assets/images/[name][hash].[ext]'
-          }
-        }]
-      },
-      {
+    rules: [{
         test: /\.styl$/,
+        exclude: /node_modules/,
         use: [
           'style-loader',
           {
             loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
             options: {
               sourceMap: true
             }
@@ -57,8 +28,29 @@ const config = {
               remPrecision: 8
             }
           },
-          'stylus-loader'
+          'postcss-loader',
+          'stylus-loader',
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(jade|pug)$/,
+        exclude: /node_modules/,
+        use: ['html-loader', 'pug-html-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'assets/images/[name][hash].[ext]'
+          }
+        }]
       }
     ]
   },
@@ -86,6 +78,25 @@ if (process.env.NODE_ENV === 'development') {
   )
 } else {
   // 生产模式
+  /* 此项是为css单独打包，将MiniCssExtractPlugin.loader按数组位置插入 */
+  config.module.rules[0].use.splice(1, 0, MiniCssExtractPlugin.loader)
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'style.[chunkhash:8].css',
+    })
+  )
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10
+        }
+      }
+    }
+  }
 }
 
 module.exports = config
