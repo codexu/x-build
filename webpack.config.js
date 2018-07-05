6cad488539054d413664f8dc444d1c2e5f88f667
李旭 <461229187@qq.com>
2018-07-05 16:17:21 +0800

    build add media font

--- webpack.config.js
+++ webpack.config.js
@@ -79,13 +79,35 @@ const config = {
         use: 'pug-loader'
       },
       {
-        test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         use: [{
           loader: 'url-loader',
           options: {
-            limit: 8192,
-            name: '[name][hash].[ext]',
+            limit: 10000,
+            name: '[name][hash:7].[ext]',
             outputPath: 'static/images/'
+          }
+        }]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: '[name][hash:7].[ext]',
+            outputPath: 'static/font/'
+          }
+        }]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: '[name][hash:7].[ext]',
+            outputPath: 'static/media/'
           }
         }]
       },
