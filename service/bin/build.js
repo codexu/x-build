ab2416c74cbee610d5c860bc6d78e9600c74be5f
codexu <461229187@qq.com>
2019-02-16 16:59:50 +0800

    fix: service for windows

--- service/bin/build.js
+++ service/bin/build.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-
+const os = require('os');
 const execute = require('../../lib/execute');
 const CMD_STR = './node_modules/.bin/webpack --config ./node_modules/x-build/service/webpack/webpack.prod.js';
+
+if (os.type() === 'Windows_NT') {
+  CMD_STR.replace(new RegExp('/', 'g'), '\\');
+}
 
 (async function () {
   await execute(CMD_STR, false, (contents) => console.log(contents));
--- service/bin/serve.js
+++ service/bin/serve.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-
+const os = require('os');
 const execute = require('../../lib/execute');
-const CMD_STR = './node_modules/.bin/webpack-dev-server --config ./node_modules/x-build/service/webpack/webpack.dev.js';
+let CMD_STR = './node_modules/.bin/webpack-dev-server --config ./node_modules/x-build/service/webpack/webpack.dev.js';
+
+if (os.type() === 'Windows_NT') {
+  CMD_STR.replace(new RegExp('/', 'g'), '\\');
+}
 
 (async function () {
   await execute(CMD_STR, true, (contents) => console.log(contents));
