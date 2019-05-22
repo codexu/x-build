daaadce6f218d98a02e175583031c60dfa2762c1
codexu <461229187@qq.com>
2019-05-22 15:30:33 +0800

    feat: remUnit config

--- service/webpack/lib/isRem.js
+++ service/webpack/lib/isRem.js
@@ -1,9 +1,11 @@
+const config = require(path.resolve(`${process.cwd()}/config.json`));
+
 const rem = (isRem) => {
   if (isRem) {
     return {
       loader: 'px2rem-loader',
       options: {
-        remUnit: 46.875,
+        remUnit: config.remUnit,
         remPrecision: 8
       }
     };
