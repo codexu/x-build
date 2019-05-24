b2538730269c696ae246e530ef1265de139fdce2
codexu <461229187@qq.com>
2019-05-24 11:37:27 +0800

    fix: api load files name

--- temp/src/api/index.js
+++ temp/src/api/index.js
@@ -2,11 +2,11 @@ const files = require.context('./', true, /\.js$/);
 const modules = {};
 
 function checkIndex(name) {
-  return name !== './index.js'
+  return name !== './index.js';
 }
 
 files.keys().filter(checkIndex).forEach(key => {
-  modules[/(?<=\/)[^\/\.js]+(?=\.js)/.exec(key)] = files(key).default;
-})
+  modules[key.replace(/(.*\/)*([^.]+).js/ig, '$2')] = files(key).default;
+});
 
 export default modules;
