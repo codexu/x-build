197f98f1c5b2f74b3bd9f65d57339677e9784489
lixu <461229187@qq.com>
2021-03-26 11:22:54 +0800

    feat: 🎸 自动初始化 Git 仓库

--- bin/creator.js
+++ bin/creator.js
@@ -9,45 +9,57 @@ const clearConsole = require('./utils/clearConsole');
 
 let startTime, endTime;
 
-module.exports = function (name) {
+module.exports = async function (name) {
   // CLI 模板文件夹路径
   const src = path.resolve(__dirname, '../template');
   // 目标路径
   const dest = path.resolve(process.cwd(), name);
+  // copy .gitignore
+  const gitignore = path.resolve(process.cwd(), name, 'gitignore.md');
+  const gitignoreDest = path.resolve(process.cwd(), name, '.gitignore');
+
+  console.log(gitignore, gitignoreDest);
 
   startTime = new Date().getTime()
-  clearConsole('cyan', `x-build v${require('../package').version}`);
+  clearConsole('cyan', `X-BUILD v${require('../package').version}`);
   console.log(`> 项目模板生成于目录： ${chalk.yellow(dest)}`);
+  // 拷贝模板文件
+  await fs.copy(src, dest);
+  await fs.move(gitignore, gitignoreDest);
+  await spawnCmd(dest, null, 'git', ['init'])
+  await spawnCmd(dest, null, 'git', ['add .'])
+  await spawnCmd(dest, null, 'git', ['commit -m "Initialize by X-BUILD"'])
+  console.log(`> 成功初始化 Git 仓库`);
   console.log(`> 正在自动安装依赖，请稍等...`);
   console.log('');
-
-  // 拷贝模板文件
-  fs.copy(src, dest).then(() => {
-    // 执行自动安装依赖
-    spawnCmd(dest, name);
-  });
+  await spawnCmd(dest, 'inherit', 'npm', ['install']);
+  clearConsole('cyan', `Mapwhale v${require('../package').version}`);
+  endTime = new Date().getTime();
+  usageTime = (endTime - startTime) / 1000
+  console.log(`> 项目已经创建成功，用时${chalk.cyan(usageTime)}s，请输入以下命令继续...`);
+  console.log('');
+  console.log(chalk.cyan(' $ ') + chalk.blueBright(`cd ${name}`));
+  console.log(chalk.cyan(' $ ') + chalk.blueBright('npm run serve'));
 };
 
 /**
  * 安装依赖指令
  * @param {string} dest 需要执行指令的路径
  */
-function spawnCmd(dest, name) {
-  const ls = spawn('npm', ['install'], {
+function spawnCmd(dest, stdio = 'inherit', cmd, instruction) {
+  const ls = spawn(cmd, instruction, {
     cwd: dest,
-    stdio: 'inherit',
+    stdio: stdio,
     shell: true
   });
-  ls.on('close', (code) => {
-    // 成功安装依赖
-    if (code === 0) {
-      clearConsole('cyan', `x-build v${require('../package').version}`);
-      endTime = new Date().getTime();
-      usageTime = (endTime - startTime) / 1000
-      console.log(`> 项目已经创建成功，用时${chalk.cyan(usageTime)}s，请输入以下命令继续...`);
-      console.log('');
-      console.log(chalk.cyan(' $ ') + chalk.blueBright(`cd ${name}`));
-      console.log(chalk.cyan(' $ ') + chalk.blueBright('npm run serve'));
-    }
-  });
+  return new Promise((resolve, reject) => {
+    ls.on('close', (code) => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject();
+      }
+    });
+  })
+
 }
