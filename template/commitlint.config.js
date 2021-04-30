dddb9df02da6cc16caac6d2983eddc8e8e8a51b9
李旭 <461229187@qq.com>
2021-04-30 11:29:36 +0800

    feat: 🎸 husky (#40)

---
+++ template/commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+  },
+};
