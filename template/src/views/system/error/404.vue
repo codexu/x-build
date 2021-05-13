614243b316b89d7cc0d472b2a03263c02f360ae6
lixu <461229187@qq.com>
2021-05-13 09:54:46 +0800

    feat: 🎸 404 倒计时跳转

--- template/src/views/system/error/404.vue
+++ template/src/views/system/error/404.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="page-error-404">
     <h1>404</h1>
+    <p>{{ countdown }} seconds after the jump...</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import router from '@/router';
 
 export default defineComponent({
   name: 'PageError404',
   setup() {
-    return {};
+    const countdown = ref(3);
+
+    const countdownInterval = setInterval(() => {
+      countdown.value -= 1;
+      if (countdown.value === 0) {
+        clearInterval(countdownInterval);
+        router.push('/');
+      }
+    }, 1000);
+
+    return { countdown };
   },
 });
 </script>
