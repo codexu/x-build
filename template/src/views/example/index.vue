893185914761f37c2f9d991ee66c8c8b96d739bc
宋雪妍 <songxueyan@mapwhale.com>
2021-06-18 15:31:21 +0800

    feat: fullscreen store

--- template/src/views/example/index.vue
+++ template/src/views/example/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="page-example">
-    {{type}}
+    {{ listen }}
+    <button @click="screen">{{ activeText }}</button>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
-import { useDeviceStore } from '@/store/sys/device';
+import { useFullscreenStore } from '@/store/sys/fullscreen';
 
 export default defineComponent({
   name: 'PageExample',
   setup() {
-    const deviceStore = useDeviceStore();
-    const type = computed(() => deviceStore.type);
+    const fullscreenStore = useFullscreenStore();
+    const active = computed(() => (fullscreenStore.active));
+    const activeText = computed(() => (active.value ? '收起' : '展开'));
+    function screen() {
+      fullscreenStore.toggle();
+    }
     return {
-      type,
+      activeText,
+      screen,
     };
   },
 });
