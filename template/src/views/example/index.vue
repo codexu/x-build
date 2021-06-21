c79432d8672f9bcaf06239f7abc8412c8f76d66b
lixu <461229187@qq.com>
2021-06-21 11:24:31 +0800

    feat: 🎸 error log store add id

--- template/src/views/example/index.vue
+++ template/src/views/example/index.vue
@@ -2,26 +2,31 @@
   <div class="page-example">
     <button @click="succeed">成功</button>
      <button @click="errors">失败</button>
+     <p v-for="error in errorLogs" :key="error.id">
+       {{ error.id }} | {{ error.error.message }} | {{ error.time }}
+     </p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 import { useLogStore } from '@/store/sys/log';
 
 export default defineComponent({
   name: 'PageExample',
   setup() {
     const logStore = useLogStore();
+    const errorLogs = computed(() => logStore.errorLogs);
     function succeed() {
       logStore.notification({ type: 'warning', message: 'aaa' });
     }
     function errors() {
-      logStore.notification(Error('失败了'));
+      logStore.notification(new Error('失败了'));
     }
     return {
       succeed,
       errors,
+      errorLogs,
     };
   },
 });
