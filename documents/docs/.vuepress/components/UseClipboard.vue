f958e3d93a38b4d26a1cfa843b74fcf64b88e081
宋雪妍 <songxueyan@mapwhale.com>
2021-06-24 13:47:54 +0800

    feat: useClipboard 复制文档

---
+++ documents/docs/.vuepress/components/UseClipboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="padding-top: 16px">
+    <el-input v-model="input" placeholder="请输入内容" style="width:200px"></el-input>
+    <el-button @click="onClick" style="margin-left:10px">复制</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import useClipboard from "../../../../template/src/hooks/useClipboard";
+import { ElButton, ElInput } from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
+
+export default defineComponent({
+  components: {
+    ElButton,
+    ElInput,
+  },
+  setup() {
+    let input = ref("复制的文本");
+    const { copyText } = useClipboard();
+    const onClick = () => {
+      copyText(input.value)
+    }
+    return { input, onClick };
+  },
+});
+</script>
+
+<style scoped>
+</style>
