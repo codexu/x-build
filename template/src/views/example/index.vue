c8633f72a7ced4d5613742d8d4719ac87df1a7c7
lixu <461229187@qq.com>
2021-06-25 13:24:05 +0800

    fix: 🐛 example

--- template/src/views/example/index.vue
+++ template/src/views/example/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="page-example">
-    {{ menu }}
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-import { useMenuStore } from '@/store/sys/menu';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageExample',
   setup() {
-    const menuStore = useMenuStore();
-    const menu = computed(() => menuStore.menu);
-    return { menu };
+    return { };
   },
 });
 </script>
