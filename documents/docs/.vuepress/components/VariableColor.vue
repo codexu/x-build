6c818b4226ecef9f9c72676a306c245bd3d81e76
宋雪妍 <songxueyan@mapwhale.com>
2021-07-01 11:00:11 +0800

    feat: 内置变量less、scss文档切换

--- documents/docs/.vuepress/components/VariableColor.vue
+++ documents/docs/.vuepress/components/VariableColor.vue
@@ -1,46 +1,85 @@
 <template>
-  <div style="padding-top: 16px">
-    <ul>
-      <li class="head">
-        <div>说明</div>
-        <div>值</div>
-        <div>变量</div>
-        <span>操作</span>
-      </li>
-      <li
-        class="size"
-        v-for="(item, index) in colorData"
-        :key="index"
-        :style="{ color: item.value }"
-        :class="item.name === '标题' ? 'colors' : ''"
-      >
-        <div>{{ item.explain }}</div>
-        <div>{{ item.value }}</div>
-        <div>{{ item.variable }}</div>
-        <span><a @click="copyText(item.variable)">复制</a></span>
-      </li>
-    </ul>
-  </div>
+  <el-tabs v-model="value">
+    <el-tab-pane label="scss" name="scss">
+      <ul>
+        <li class="head">
+          <div>说明</div>
+          <div>值</div>
+          <div>变量</div>
+          <span>操作</span>
+        </li>
+        <li
+          class="size"
+          v-for="(item, index) in colorDataScss"
+          :key="index"
+          :style="{ color: item.value }"
+          :class="item.name === '标题' ? 'colors' : ''"
+        >
+          <div>{{ item.explain }}</div>
+          <div>{{ item.value }}</div>
+          <div>{{ item.variable }}</div>
+          <span><a @click="copyText(item.variable)">复制</a></span>
+        </li>
+      </ul>
+    </el-tab-pane>
+    <el-tab-pane label="less" name="less">
+      <ul>
+        <li class="head">
+          <div>说明</div>
+          <div>值</div>
+          <div>变量</div>
+          <span>操作</span>
+        </li>
+        <li
+          class="size"
+          v-for="(item, index) in colorDataLess"
+          :key="index"
+          :style="{ color: item.value }"
+          :class="item.name === '标题' ? 'colors' : ''"
+        >
+          <div>{{ item.explain }}</div>
+          <div>{{ item.value }}</div>
+          <div>{{ item.variable }}</div>
+          <span><a @click="copyText(item.variable)">复制</a></span>
+        </li>
+      </ul>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import useClipboard from "../hooks/useClipboard";
-
+import { defineComponent, reactive, ref } from 'vue';
+import useClipboard from '../hooks/useClipboard';
+import { ElTabs, ElTabPane } from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 export default defineComponent({
+  components: { ElTabs, ElTabPane },
   setup() {
     let { copyText } = useClipboard();
-    const colorData = reactive([
-      { explain: "主色调", value: "#1BA2FF", variable: "$primary-color" },
-      { explain: "成功", value: "#24CBB2", variable: "$success-color" },
-      { explain: "失败", value: "#DF0140", variable: "$error-color" },
-      { explain: "警告", value: "#FAE053", variable: "$warning-color" },
-      { explain: "正文", value: "#051724", variable: "$text-color" },
-      { explain: "标题", value: "$text-color", variable: "$title-color" },
-      { explain: "注释", value: "#CDD0D3", variable: "$notes-color" },
-      { explain: "数字", value: "#1BA2FF", variable: "$number-color" },
+    let value = ref('scss');
+    const colorDataScss = reactive([
+      { explain: '主色调', value: '#1BA2FF', variable: '$primary-color' },
+      { explain: '成功', value: '#24CBB2', variable: '$success-color' },
+      { explain: '失败', value: '#DF0140', variable: '$error-color' },
+      { explain: '警告', value: '#FAE053', variable: '$warning-color' },
+      { explain: '正文', value: '#051724', variable: '$text-color' },
+      { explain: '标题', value: '$text-color', variable: '$title-color' },
+      { explain: '注释', value: '#CDD0D3', variable: '$notes-color' },
+      { explain: '数字', value: '#1BA2FF', variable: '$number-color' },
     ]);
-    return { colorData, copyText };
+
+    const colorDataLess = reactive([
+      { explain: '主色调', value: '#1BA2FF', variable: '@primary-color' },
+      { explain: '成功', value: '#24CBB2', variable: '@success-color' },
+      { explain: '失败', value: '#DF0140', variable: '@error-color' },
+      { explain: '警告', value: '#FAE053', variable: '@warning-color' },
+      { explain: '正文', value: '#051724', variable: '@text-color' },
+      { explain: '标题', value: '@text-color', variable: '@title-color' },
+      { explain: '注释', value: '#CDD0D3', variable: '@notes-color' },
+      { explain: '数字', value: '#1BA2FF', variable: '@number-color' },
+    ]);
+
+    return { colorDataScss, colorDataLess, value, copyText };
   },
 });
 </script>
@@ -73,5 +112,20 @@ ul {
       font-weight: bold;
     }
   }
+}
+::v-deep .el-tabs__header {
+  margin: 0;
+  .el-tabs__item {
+    &:hover {
+      color: var(--c-text-accent);
+    }
+    &.is-active {
+      color: var(--c-text-accent);
+    }
+  }
+}
+
+::v-deep .el-tabs__active-bar {
+  background-color: var(--c-text-accent);
 }
 </style>
--- documents/docs/.vuepress/components/VariableSize.vue
+++ documents/docs/.vuepress/components/VariableSize.vue
@@ -1,96 +1,183 @@
 <template>
-  <div style="padding-top: 16px">
-    <ul>
-      <li class="head">
-        <div>说明</div>
-        <div>值</div>
-        <div>变量</div>
-        <span>操作</span>
-      </li>
-      <li
-        class="size"
-        v-for="(item, index) in sizeData"
-        :key="index"
-        :style="{ 'font-size': item.value }"
-      >
-        <div>{{ item.explain }}</div>
-        <div>{{ item.value }}</div>
-        <div>{{ item.variable }}</div>
-        <span><a @click="copyText(item.variable)">复制</a></span>
-      </li>
-      <br />
-      <li class="title-h1">
-        <h1>标题 H1</h1>
-        <h1>26px</h1>
-        <h1>$heading-1-size</h1>
-        <span><a @click="copyText('$heading-1-size')">复制</a></span>
-      </li>
-      <li class="title-h2">
-        <h2>标题 H2</h2>
-        <h2>24px</h2>
-        <h2>$heading-2-size</h2>
-        <span><a @click="copyText('$heading-2-size')">复制</a></span>
-      </li>
-      <li class="title-h3">
-        <h3>标题 H3</h3>
-        <h3>22px</h3>
-        <h3>$heading-3-size</h3>
-        <span><a @click="copyText('$heading-3-size')">复制</a></span>
-      </li>
-      <li class="title-h4">
-        <h4>标题 H4</h4>
-        <h4>20px</h4>
-        <h4>$heading-4-size</h4>
-        <span><a @click="copyText('$heading-4-size')">复制</a></span>
-      </li>
-      <li class="title-h5">
-        <h5>标题 H5</h5>
-        <h5>18px</h5>
-        <h5>$heading-5-size</h5>
-        <span><a @click="copyText('$heading-5-size')">复制</a></span>
-      </li>
-      <li class="title-h6">
-        <h6>标题 H6</h6>
-        <h6>16px</h6>
-        <h6>$heading-6-size</h6>
-        <span><a @click="copyText('$heading-6-size')">复制</a></span>
-      </li>
-      <br />
-      <li>
-        <div>行高</div>
-        <div>2</div>
-        <div>$line-height-base</div>
-        <span><a @click="copyText('$line-height-base')">复制</a></span>
-      </li>
-      <li>
-        <div>字间距</div>
-        <div>2</div>
-        <div>$letter-space-base</div>
-        <span><a @click="copyText('$letter-space-base')">复制</a></span>
-      </li>
-      <li>
-        <div>默认字体</div>
-        <div>Microsoft YaHei,PingFang SC</div>
-        <div>$font-family-base</div>
-        <span><a @click="copyText('$font-family-base')">复制</a></span>
-      </li>
-    </ul>
-  </div>
+  <el-tabs v-model="value">
+    <el-tab-pane label="scss" name="scss">
+      <ul>
+        <li class="head">
+          <div>说明</div>
+          <div>值</div>
+          <div>变量</div>
+          <span>操作</span>
+        </li>
+        <li
+          class="size"
+          v-for="(item, index) in sizeDataScss"
+          :key="index"
+          :style="{ 'font-size': item.value }"
+        >
+          <div>{{ item.explain }}</div>
+          <div>{{ item.value }}</div>
+          <div>{{ item.variable }}</div>
+          <span><a @click="copyText(item.variable)">复制</a></span>
+        </li>
+        <br />
+        <li class="title-h1">
+          <h1>标题 H1</h1>
+          <h1>26px</h1>
+          <h1>$heading-1-size</h1>
+          <span><a @click="copyText('$heading-1-size')">复制</a></span>
+        </li>
+        <li class="title-h2">
+          <h2>标题 H2</h2>
+          <h2>24px</h2>
+          <h2>$heading-2-size</h2>
+          <span><a @click="copyText('$heading-2-size')">复制</a></span>
+        </li>
+        <li class="title-h3">
+          <h3>标题 H3</h3>
+          <h3>22px</h3>
+          <h3>$heading-3-size</h3>
+          <span><a @click="copyText('$heading-3-size')">复制</a></span>
+        </li>
+        <li class="title-h4">
+          <h4>标题 H4</h4>
+          <h4>20px</h4>
+          <h4>$heading-4-size</h4>
+          <span><a @click="copyText('$heading-4-size')">复制</a></span>
+        </li>
+        <li class="title-h5">
+          <h5>标题 H5</h5>
+          <h5>18px</h5>
+          <h5>$heading-5-size</h5>
+          <span><a @click="copyText('$heading-5-size')">复制</a></span>
+        </li>
+        <li class="title-h6">
+          <h6>标题 H6</h6>
+          <h6>16px</h6>
+          <h6>$heading-6-size</h6>
+          <span><a @click="copyText('$heading-6-size')">复制</a></span>
+        </li>
+        <br />
+        <li>
+          <div>行高</div>
+          <div>2</div>
+          <div>$line-height-base</div>
+          <span><a @click="copyText('$line-height-base')">复制</a></span>
+        </li>
+        <li>
+          <div>字间距</div>
+          <div>2</div>
+          <div>$letter-space-base</div>
+          <span><a @click="copyText('$letter-space-base')">复制</a></span>
+        </li>
+        <li>
+          <div>默认字体</div>
+          <div>Microsoft YaHei,PingFang SC</div>
+          <div>$font-family-base</div>
+          <span><a @click="copyText('$font-family-base')">复制</a></span>
+        </li>
+      </ul>
+    </el-tab-pane>
+    <el-tab-pane label="less" name="less">
+      <ul>
+        <li class="head">
+          <div>说明</div>
+          <div>值</div>
+          <div>变量</div>
+          <span>操作</span>
+        </li>
+        <li
+          class="size"
+          v-for="(item, index) in sizeDataLess"
+          :key="index"
+          :style="{ 'font-size': item.value }"
+        >
+          <div>{{ item.explain }}</div>
+          <div>{{ item.value }}</div>
+          <div>{{ item.variable }}</div>
+          <span><a @click="copyText(item.variable)">复制</a></span>
+        </li>
+        <br />
+        <li class="title-h1">
+          <h1>标题 H1</h1>
+          <h1>26px</h1>
+          <h1>@heading-1-size</h1>
+          <span><a @click="copyText('@heading-1-size')">复制</a></span>
+        </li>
+        <li class="title-h2">
+          <h2>标题 H2</h2>
+          <h2>24px</h2>
+          <h2>@heading-2-size</h2>
+          <span><a @click="copyText('@heading-2-size')">复制</a></span>
+        </li>
+        <li class="title-h3">
+          <h3>标题 H3</h3>
+          <h3>22px</h3>
+          <h3>@heading-3-size</h3>
+          <span><a @click="copyText('@heading-3-size')">复制</a></span>
+        </li>
+        <li class="title-h4">
+          <h4>标题 H4</h4>
+          <h4>20px</h4>
+          <h4>@heading-4-size</h4>
+          <span><a @click="copyText('@heading-4-size')">复制</a></span>
+        </li>
+        <li class="title-h5">
+          <h5>标题 H5</h5>
+          <h5>18px</h5>
+          <h5>@heading-5-size</h5>
+          <span><a @click="copyText('@heading-5-size')">复制</a></span>
+        </li>
+        <li class="title-h6">
+          <h6>标题 H6</h6>
+          <h6>16px</h6>
+          <h6>@heading-6-size</h6>
+          <span><a @click="copyText('@heading-6-size')">复制</a></span>
+        </li>
+        <br />
+        <li>
+          <div>行高</div>
+          <div>2</div>
+          <div>@line-height-base</div>
+          <span><a @click="copyText('@line-height-base')">复制</a></span>
+        </li>
+        <li>
+          <div>字间距</div>
+          <div>2</div>
+          <div>@letter-space-base</div>
+          <span><a @click="copyText('@letter-space-base')">复制</a></span>
+        </li>
+        <li>
+          <div>默认字体</div>
+          <div>Microsoft YaHei,PingFang SC</div>
+          <div>@font-family-base</div>
+          <span><a @click="copyText('@font-family-base')">复制</a></span>
+        </li>
+      </ul>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import useClipboard from "../hooks/useClipboard";
-
+import { defineComponent, reactive, ref } from 'vue';
+import useClipboard from '../hooks/useClipboard';
+import { ElTabs, ElTabPane } from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 export default defineComponent({
+  components: { ElTabs, ElTabPane },
   setup() {
     let { copyText } = useClipboard();
-    const sizeData = reactive([
-      { explain: "辅助文字", value: "12px", variable: "$font-size-sm" },
-      { explain: "正文", value: "14px", variable: "$font-size-base" },
-      { explain: "大号文字", value: "16px", variable: "$font-size-base" },
+    let value = ref('scss');
+    const sizeDataScss = reactive([
+      { explain: '辅助文字', value: '12px', variable: '$font-size-sm' },
+      { explain: '正文', value: '14px', variable: '$font-size-base' },
+      { explain: '大号文字', value: '16px', variable: '$font-size-base' },
     ]);
-    return { sizeData, copyText };
+    const sizeDataLess = reactive([
+      { explain: '辅助文字', value: '12px', variable: '@font-size-sm' },
+      { explain: '正文', value: '14px', variable: '@font-size-base' },
+      { explain: '大号文字', value: '16px', variable: '@font-size-base' },
+    ]);
+    return { sizeDataScss, sizeDataLess, value, copyText };
   },
 });
 </script>
@@ -151,5 +238,21 @@ ul {
   h6 {
     font-size: $heading-6-size;
   }
+}
+::v-deep .el-tabs__header {
+  margin: 0;
+   .el-tabs__item {
+  &:hover {
+    color: var(--c-text-accent);
+  }
+  &.is-active {
+    color: var(--c-text-accent);
+  }
+}
+}
+
+
+::v-deep .el-tabs__active-bar {
+  background-color: var(--c-text-accent);
 }
 </style>
