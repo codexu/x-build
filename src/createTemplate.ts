d8bd3d585990d0b98b054137c3e01009dc1dc0db
lixu <461229187@qq.com>
2021-07-02 15:29:57 +0800

    feat: 🎸 add package

--- src/createTemplate.ts
+++ src/createTemplate.ts
@@ -4,26 +4,30 @@ import path = require("path");
 import prettier = require("prettier");
 import options from "./options";
 
-export default function (templatePath: string) {
-  return async function (src: string): Promise<void> {
-    const file = path.parse(src);
-    const readFilePath = path.resolve(templatePath, file.dir, `${file.name}.ejs`);
-    const outputFilePath = path.resolve(templatePath, src);
+export async function ejsRender (filePath: string): Promise<void> {
+  const file = path.parse(filePath);
+  const readFilePath = path.resolve(options.dest, file.dir, `${file.name}.ejs`);
+  const outputFilePath = path.resolve(options.dest, filePath);
 
-    const templateCode = await fs.readFile(readFilePath);
+  const templateCode = await fs.readFile(readFilePath);
 
-    const code = ejs.render(templateCode.toString(), options);
-    const extname = path.extname(src).replace(/[.]/g, '');
-    let prettierCode: string
-    if (extname === 'ts' || extname === 'js') {
-      await prettier.resolveConfig(options.src).then((options) => {
-        prettierCode = prettier.format(code, options);
-      });
-    } else {
-      prettierCode = prettier.format(code, { parser: extname });
-    }
-
-    await fs.outputFile(outputFilePath, prettierCode)
-    await fs.remove(readFilePath)
+  const code = ejs.render(templateCode.toString(), options);
+  const extname = path.extname(filePath).replace(/[.]/g, '');
+  let prettierCode: string
+  if (extname === 'ts' || extname === 'js') {
+    await prettier.resolveConfig(options.src).then((options) => {
+      prettierCode = prettier.format(code, options);
+    });
+  } else {
+    prettierCode = prettier.format(code, { parser: extname });
   }
+
+  await fs.outputFile(outputFilePath, prettierCode)
+  await fs.remove(readFilePath)
+}
+
+export async function packageRender(filePath: string): Promise<void> {
+  const src = path.resolve(__dirname, '../package', filePath);
+  const dest = path.resolve(options.dest, 'src', filePath);
+  await fs.copy(src, dest);
 }
