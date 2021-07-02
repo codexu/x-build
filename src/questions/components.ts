40e1d656207a78c3bedb5f6a29f2c3e3f8b1ebd9
lixu <461229187@qq.com>
2021-07-02 10:04:32 +0800

    feat: 🎸 question add device

--- src/questions/components.ts
+++ src/questions/components.ts
@@ -1,10 +1,18 @@
-export default {
+export const pcComponents = {
   name: 'components',
   type: 'list',
-  message: '选择使用的组件库',
+  message: '选择 UI 框架',
   choices: [
     { name: 'Element Plus', value: 'element' },
     { name: 'Ant Design Vue 2.x', value: 'antd' },
-    { name: '其他', value: '' },
+    { name: 'Vuetify 3.x', value: 'vuetify' },
+  ],
+}
+export const mobileComponents = {
+  name: 'components',
+  type: 'list',
+  message: '选择 UI 框架',
+  choices: [
+    { name: 'Vant', value: 'vant' },
   ],
 }
