579f06d4eb53ba3503a2b7ce7c0b7395cd7f3976
lixu <461229187@qq.com>
2021-09-03 10:23:24 +0800

    feat: 🎸 tailwind

---
+++ template/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
