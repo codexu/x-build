0668d9eb6350d5ac7d40963efda7d495c060cfad
codexu <461229187@qq.com>
2022-02-25 02:21:23 +0000

    Deploying to gh-pages from  @ f785a0677ad864c8057590d222774da378ea3da7 🚀

---
+++ assets/i18n.html.a1c8f83e.js
@@ -0,0 +1,1 @@
+const e={key:"v-ecaf6aa8",path:"/guide/ui/i18n.html",title:"\u56FD\u9645\u5316",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1645755528e3,contributors:[{name:"codexu",email:"461229187@qq.com",commits:1}]},filePathRelative:"guide/ui/i18n.md"};export{e as data};
